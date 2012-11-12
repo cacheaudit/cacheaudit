9187c5ef6c25ed442c7c5f81f64e8ed8605cd86d
Jan Reineke <reineke@cs.uni-saarland.de>
2012-11-12 18:26:54 +0000

    test

--- CaseStudies/Illustration/preloadingPlusUnknown/unknown3.c
+++ CaseStudies/Illustration/preloadingPlusUnknown/unknown3.c
@@ -3,13 +3,13 @@
 int main() {
         unsigned long A[arrsize];
 	unsigned long B[arrsize];
-	register unsigned int secret asm ("ebx");
+	unsigned int secret;
         register unsigned int temp asm("ecx");
 	register unsigned int i asm("edx");
 
         for (i = 0; i < arrsize; i++)
-                temp = A[i];
+        	temp = A[i];
 
         if (secret < arrsize)
-	  temp = B[secret];
+		temp = B[secret];
 }
