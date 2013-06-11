3bf9ee6e097a2e957131667264d68812ab355c5b
Boris Koepf <boris.koepf@gmail.com>
2013-06-11 16:20:40 +0000

    Cut down interfaces

--- iterator/memAD.mli
+++ iterator/memAD.mli
@@ -1,35 +1,3 @@
-val time_instr : int
-val time_test : int
-val time_effective_load : int
-module MemSet :
-  sig
-    type elt = Int64.t
-    type t = Set.Make(Int64).t
-    val empty : t
-    val is_empty : t -> bool
-    val mem : elt -> t -> bool
-    val add : elt -> t -> t
-    val singleton : elt -> t
-    val remove : elt -> t -> t
-    val union : t -> t -> t
-    val inter : t -> t -> t
-    val diff : t -> t -> t
-    val compare : t -> t -> int
-    val equal : t -> t -> bool
-    val subset : t -> t -> bool
-    val iter : (elt -> unit) -> t -> unit
-    val fold : (elt -> 'a -> 'a) -> t -> 'a -> 'a
-    val for_all : (elt -> bool) -> t -> bool
-    val exists : (elt -> bool) -> t -> bool
-    val filter : (elt -> bool) -> t -> t
-    val partition : (elt -> bool) -> t -> t * t
-    val cardinal : t -> int
-    val elements : t -> elt list
-    val min_elt : t -> elt
-    val max_elt : t -> elt
-    val choose : t -> elt
-    val split : elt -> t -> t * bool * t
-  end
 module MemAD :
   functor (F : Signatures.FLAG_ABSTRACT_DOMAIN) ->
     functor (T : Signatures.TRACE_ABSTRACT_DOMAIN) ->
