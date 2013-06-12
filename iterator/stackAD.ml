87b445530da48ca4f996e9b25c282f00660a750e
Boris Koepf <boris.koepf@gmail.com>
2013-06-12 14:36:06 +0000

    StackAD

--- iterator/stackAD.ml
+++ iterator/stackAD.ml
@@ -1,9 +1,33 @@
 open Signatures
 open X86Types
 
+
+module type T = 
+  sig
+    include ABSTRACT_DOMAIN
+
+    val init :
+      X86Headers.t ->
+      (X86Types.reg32 * int64 * int64) list -> cache_param -> t
+    val get_offset : t -> op32 -> (int, t) finite_set
+    val test : t -> X86Types.condition -> t add_bottom * t add_bottom
+    val call : t -> op32 -> int -> (int, t) finite_set
+    val return : t -> (int, t) finite_set
+    val memop : t -> memop -> op32 -> op32 -> t
+    val memopb : t -> memop -> op8 -> op8 -> t
+    val movzx : t -> op32 -> op8 -> t
+    val load_address : t -> X86Types.reg32 -> X86Types.address -> t
+    val flagop : t -> op32 flagop -> t
+    val stackop : t -> stackop -> op32 -> t
+    val shift : t -> X86Types.shift_op -> op32 -> op8 -> t
+    val elapse : t -> int -> t
+    val access_readonly : t -> int64 -> t
+  end
+
+
 (* Simple stack abstract domain. Translates pushs and pops to register operations *)
 
-module StackAD (M: MEMORY_ABSTRACT_DOMAIN) : STACK_ABSTRACT_DOMAIN = struct
+module Make (M: MEMORY_ABSTRACT_DOMAIN) = struct
     (* Stack.top and Memory abstract domain *)
   type t = M.t
 
