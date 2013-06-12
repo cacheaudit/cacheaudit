1f9ca55e4e3ced5c22da47e926e3db1935248943
Dominik Feld <dfeld@software.imdea.org>
2013-06-12 10:19:22 +0000

    cleaning up

--- iterator/ageFunction.ml
+++ iterator/ageFunction.ml
@@ -16,83 +16,16 @@ module type AGE_FUNCTION = sig
   val empty : t
   (* Returns the most recent binding for the variable. *)
   val get : var -> t -> int
-
+  (* <project af l> Returns an age function only consisting of variables in l. *)
+  (* l is expected to contain only variables in used in af.vars *)
   val project : t -> var list -> t
   (* Given two maps with distinct input spaces, it returns a mapping that contains the mapping of both AgeFunctions. *)
   val join : t -> t -> t
   (* Returns a string representation of the AgeFunction. *)
   val toString : t -> string
+  (* Returns the set of variables for which there exists a mapping. *)
   val vars : t -> VarSet.t
 end
-
-(* An implementation of an AgeFunction using Maps *)
-module MapAgeFunction : AGE_FUNCTION = struct
-  type t = int AgeMap.t
-
-  let add v i af = AgeMap.add v i af
-  let compare af1 af2 = Pervasives.compare af1 af2(*AgeMap.compare (fun age1 age2 -> Pervasives.compare age1 age2) af1 af2*)
-      
-  let get v af = AgeMap.find v af
-
-  let join af1 af2 = AgeMap.merge (fun v opt1 opt2 -> match opt1,opt2 with 
-              Some x,None -> Some x | None, Some x -> Some x | _,_ -> failwith "AF.join: unexpected case") af1 af2
-  let empty = AgeMap.empty
-
-  let project af l = List.fold_left (fun af' v -> add v (get v af) af') empty l
-
-  let toString af = 
-    let s = AgeMap.fold (fun v e s -> s ^ (string_of_int e) ^ ",") af "" in
-    "(" ^ (String.sub s 0 (String.length s -1)) ^ ")" 
-
-  let vars af = 
-    List.fold_left (fun varset (v,i) -> VarSet.add v varset) VarSet.empty (AgeMap.bindings af)
-end
-
-(* An implementation of an AgeFunction using Lists *)
-module ListAgeFunction : AGE_FUNCTION = struct
-  type t = {vals : int list; vars : VarSet.t}
-  
-  let compare af1 af2 : int = Pervasives.compare af1.vals af2.vals
-    (*let c = Pervasives.compare af1.vars af2.vars in(*VarSet.compare af1.vars af2.vars in*)
-    if c = 0 then 
-      Pervasives.compare af1.vals af2.vals
-    else 
-      c*)
-
-  (* insert element e at position n in the list l *)
-  let rec list_insert (n:int) (l: 'a list) (e: 'a) : 'a list = 
-    match n with 0 -> e :: l | m -> List.hd l :: list_insert (n-1) (List.tl l) e
-
-  (* replace the element at position n in the list l with element e *)
-  let rec list_replace (n:int) (l: 'a list) (e: 'a) : 'a list = 
-    match n with 0 -> e :: (List.tl l) | m -> List.hd l :: list_replace (n-1) (List.tl l) e
-
-  (* Computes the position of v in the set or the position where it would get inserted (if it is no element of set) *)
-  let pos set v = 
-    match VarSet.split v set with (l,e,g) -> VarSet.cardinal l
-
-  let empty = {vals = []; vars = VarSet.empty}
-
-  let get v af = let pos_v = pos af.vars v in
-    List.nth af.vals pos_v
-
-  let add v i af = 
-    if VarSet.mem v af.vars then
-      {af with vals = list_replace (pos af.vars v) af.vals i}
-    else
-      {vals = list_insert (pos af.vars v) af.vals i; vars = VarSet.add v af.vars}
-
-  let join af1 af2 = VarSet.fold (fun v af' -> add v (get v af2) af') af2.vars af1
-
-  let project af l = List.fold_left (fun af' v -> add v (get v af) af') empty l
-
-  let toString af = 
-    let s = List.fold_left (fun s e -> s ^ (string_of_int e) ^ ",") "" af.vals in
-    "(" ^ (String.sub s 0 (String.length s -1)) ^ ")"  
-
-  let vars af = af.vars
-end
-
 
 module PairListAgeFunction : AGE_FUNCTION = struct
   type t = (var * int) list
@@ -116,7 +49,7 @@ module PairListAgeFunction : AGE_FUNCTION = struct
                      -1 -> (v,i)::af
                   |   0 -> (v,i)::tl
                   |   1 -> (v',i')::add v i tl
-                  |   _ -> failwith "ageFunction: Unimplemented case"
+                  |   _ -> failwith "Unexpected case in AgeFunction.add"
 
   let rec join af1 af2 = 
     match af1,af2 with
@@ -124,12 +57,7 @@ module PairListAgeFunction : AGE_FUNCTION = struct
   | (v1,i1)::t1,(v2,i2)::t2 -> let cmp = Pervasives.compare v1 v2 in
     if  cmp < 0 then (v1,i1)::(join t1 af2)
     else if cmp > 0 then (v2,i2)::(join af1 t2)
-    else failwith "bla"
-
-(*let join af1 af2 = if i1 = i2 then (v1,i1)::(join t1 af2)
-    List.fold_left (fun l (v,i) -> add v i l) af1 af2
-
-List.fold_left (fun af' v -> add v (get v af) af') empty l*)
+    else failwith "Unexpected case in AgeFunction.join"
 
   let toString af = 
     let s = List.fold_left (fun s (_,e) -> s ^ (string_of_int e) ^ ",") "" af in
@@ -140,9 +68,9 @@ List.fold_left (fun af' v -> add v (get v af) af') empty l*)
     |  (v1,i)::tl1,v2::tl2 -> let cmp = Pervasives.compare v1 v2 in
        if cmp = 0 then (v1,i)::project tl1 tl2
        else if cmp < 0 then project tl1 l
-       else failwith "unsafe project1"
+       else failwith "Unexpected case in AgeFunction.project"
     |    _, [] -> []
-    |    _,_ -> failwith "unsafe project2"
+    |    _,_ -> failwith "Unexpected case in AgeFunction.project"
 
   let rec vars af = match af with
      [] -> VarSet.empty
