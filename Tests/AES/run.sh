c852b34245c7c8648caf2e4f6fb6c5ba4cc98372
Guillermo Guridi <erpheus@gmail.com>
2013-06-14 09:56:08 +0000

    Tests (AES is incomplete and BubbleSort commented out because of its running time)

--- Tests/AES/run.sh
+++ Tests/AES/run.sh
@@ -3,16 +3,15 @@ if [ ! -f ../../cachecow ]; then
     echo "Cachetool not compiled!";
     exit 1;
 fi
-#../../cachecow --analyze --noInstructionCache bubbleSort
+../../cachecow --analyze --noInstructionCache aes_inlined
 if [ ! -f log.txt ]; then
 	echo "Log.txt not found"
     exit 1;
 fi
 cat log.txt | sed 's/^...........//' > result.txt
-sort -n result.txt -o correct.txt
 cmp correct.txt result.txt &> /dev/null
 result=$?
-rm -f correct.txt result.txt log.txt
+rm -f result.txt log.txt
 if [ $result -eq 0 ]; then
 	echo "test ok" 1>&2;
 else
