d9168c3623078cda4497454baf25148f10c62c3c
Guillermo Guridi <erpheus@gmail.com>
2013-06-17 14:35:07 +0000

    Updated tests to work with the log files. And enhanced makefile compilation if octagon has not been installed.

--- Tests/BubbleSort/run.sh
+++ Tests/BubbleSort/run.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-if [ ! -f ../../cachecow ]; then
-    echo "Cachetool not compiled!";
-    exit 1;
-fi
-../../cachecow --analyze --noInstructionCache bubbleSort
-if [ ! -f log.txt ]; then
-	echo "Log.txt not found"
-    exit 1;
-fi
-cat log.txt | sed 's/^...........//' > result.txt
+source ../common.sh
+
+runCacheCow bubblesort
+createResult
+
 sort -n result.txt -o correct.txt
 cmp correct.txt result.txt &> /dev/null
 result=$?
-rm -f correct.txt result.txt log.txt
+rm -f correct.txt result.txt log.*
 if [ $result -eq 0 ]; then
 	echo "test ok" 1>&2;
 else
