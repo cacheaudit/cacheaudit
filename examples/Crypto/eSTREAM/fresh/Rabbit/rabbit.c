86d5df72695dfb2c608e45d04b593bc6102d75c1
Goran Doychev <g.doychev@gmail.com>
2014-03-29 10:36:21 +0000

    more reorganization + supporting a missing XOR instruction

--- examples/Crypto/eSTREAM/fresh/Rabbit/rabbit.c
+++ examples/Crypto/eSTREAM/fresh/Rabbit/rabbit.c
@@ -29,17 +29,6 @@
 #include "ecrypt-portable.h"
 
 /* -------------------------------------------------------------------------- */
-// to = from AND val
-#define AND(to,from,val){\
-   asm(\
-       "and %2, %1;"\
-       "movl %1, %0;"\
-	   : "=r" (to)\
-	   : "r" (from),\
-	     "i" (val)\
-	);\
-}
-
 
 /* Square a 32-bit unsigned integer to obtain the 64-bit result and return */
 /* the upper 32 bits XOR the lower 32 bits */
@@ -135,29 +124,10 @@ void ECRYPT_keysetup(ECRYPT_ctx* ctx, const u8* key, u32 keysize, u32 ivsize)
    ctx->master_ctx.c[2] = ROTL32(k3, 16);
    ctx->master_ctx.c[4] = ROTL32(k0, 16);
    ctx->master_ctx.c[6] = ROTL32(k1, 16);
-//    u32 t1;
-//    t1 = k0&0xFFFFF000;
-//    t1 = k0&0xFFF;
-//    static const unsigned int bla = 0xFFFF0000;
-//    asm(
-//        "and %2, %1;"
-//        "movl %1, %0;"
-// 	   : "=r" (t1)
-// 	   : "r" (k0),
-// 	     "i" (bla) 
-//    );
-   
-//    AND(t1,k0,0xFFFF0000);
-   
-   u32 a1,a2;
-   AND(a1,k0,0xFFFF0000); AND(a2,k1,0xFFFF);
-   ctx->master_ctx.c[1] = a1 | a2;
-   AND(a1,k1,0xFFFF0000); AND(a2,k2,0xFFFF);
-   ctx->master_ctx.c[3] = a1 | a2;
-   AND(a1,k2,0xFFFF0000); AND(a2,k3,0xFFFF);
-   ctx->master_ctx.c[5] = a1 | a2;
-   AND(a1,k3,0xFFFF0000); AND(a2,k0,0xFFFF);
-   ctx->master_ctx.c[7] = a1 | a2;
+   ctx->master_ctx.c[1] = (k0&0xFFFF0000) | (k1&0xFFFF);
+   ctx->master_ctx.c[3] = (k1&0xFFFF0000) | (k2&0xFFFF);
+   ctx->master_ctx.c[5] = (k2&0xFFFF0000) | (k3&0xFFFF);
+   ctx->master_ctx.c[7] = (k3&0xFFFF0000) | (k0&0xFFFF);
 
    /* Clear carry bit */
    ctx->master_ctx.carry = 0;
