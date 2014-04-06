6eedfcf2a2f11cbd8956903968f768ceea1a9bc9
Goran Doychev <g.doychev@gmail.com>
2014-04-06 18:20:55 +0000

    reordering, formatting

--- abstract_domains/memAD.ml
+++ abstract_domains/memAD.ml
@@ -1,4 +1,5 @@
-let time_instr = 1 (* number of cycles of one instriction, to which we add the time for memory accesses *)
+(* times added to the time for memory accesses *)
+let time_instr = 1 (* number of cycles of one instriction *)
 let time_test = 1 (* number of cycles for a test *)
 let time_effective_load = 0; 
 
@@ -11,29 +12,22 @@ open Logger
 (* Operand type, which may be a 8 or 32-bit operand*)
 type op_t = Op8 of X86Types.op8 | Op32 of X86Types.op32
 
-(** List of initial values for registers. Register * lower bound * upper bound *)
+(* Type to differentiate memory reads from writes *)
+type rw_t = Read | Write
+
+(* List of initial values for registers *)
 type reg_init_values = (X86Types.reg32 * int64 * int64) list
 
-(** List of initial values for memory addresses. Adress * lower bound * upper bound *)
+(* List of initial values for memory addresses  *)
 type mem_init_values = (int64 * int64 * int64) list
 
-(** Parameters for the Memory Abstract Domain initialization *)
+(* Parameters for the Memory Abstract Domain initialization *)
 type mem_param = mem_init_values * reg_init_values
 
 module type S =
   sig
     include AD.S
-  
-  (* init is used to return an initial abstract state *)
-  (* the first arguments returns the initial value at a given address if it *)
-  (* is defined, None otherwise (meaning it can be any) *)
   val init: (int64 -> int64 option) -> mem_param -> CacheAD.cache_param -> t
-
-  (* from a genop32 expression, returns a finite list of possible values,
-     each value associated with an approximation of the corresponding memory 
-     states leading to that particular value. In case no finite list can be
-     determied, returns Top.
-  *)
   val get_vals: t -> op32 -> (int,t) finite_set
   val test : t -> condition -> (t add_bottom)*(t add_bottom)
   val interpret_instruction : t -> X86Types.instr -> t
@@ -41,8 +35,6 @@ module type S =
   val elapse : t -> int -> t
 end
   
-
-
 
 
 let logged_addresses = ref []
@@ -56,15 +48,16 @@ module MemSet = Set.Make(Int64)
 module Make (F : FlagAD.S) (C:CacheAD.S) = struct
 
   type t = {
-          vals : F.t; (** Element of the Value Abstract Domain *)
-          memory : MemSet.t; (** Set that contains the memory addresses used *)
-          initial_values : int64 -> (int64 option); (** Function to determine
-          the initial value of an address *)
+          vals : F.t; (* storing values of variables *)
+          memory : MemSet.t; (* set storing the memory addresses used *)
+          initial_values : int64 -> (int64 option); 
           cache : C.t;
   }
-  (** Main type for this module. *)
+  
+  (*** Printing and logging ***)
 
-  let pp_vars fmt v = MemSet.iter (Format.fprintf fmt "%a, @," X86Print.pp_addr) v
+  let pp_vars fmt v = 
+    MemSet.iter (Format.fprintf fmt "%a, @," X86Print.pp_addr) v
 
   let log_vars mem = List.iter (F.log_var mem.vals) !logged_addresses
 
@@ -93,30 +86,23 @@ module Make (F : FlagAD.S) (C:CacheAD.S) = struct
       (F.print_delta mem1.vals) mem2.vals
       (C.print_delta mem1.cache) mem2.cache
       
+  (*** Initialization ***)
   
-      
-  (** Type to differentiate memory reads from writes *)
-  type rw_t = Read | Write
-
-
-  (* Init functions for register map *)
-  (* reg_to_var: X86Types.reg32 -> int64 *)
-  (** Returns the variable number that corresponds to a given register *)
+  (* Returns the variable number that corresponds to a given register *)
   let reg_to_var x = Int64.of_int (-(X86Util.reg32_to_int x) - 1)
 
-  (** Gives variables a specific name if they have one (i.e. registers) *)
+  (* Gives variables a specific name if they have one (i.e. registers) *)
   let var_to_string v = match v with
     n -> try(
         X86Util.reg32_to_string (X86Util.int_to_reg32(-1-(Int64.to_int n)))
       ) with Invalid_argument "int_to_reg32" -> Format.sprintf "V%Lx" n
 
-  (* initRegs : F.t -> F.t *)
-  (** Adds variables and values for the registers in the value domain *)
+  (* Adds variables and values for the registers in the value domain *)
   let initRegs v regList = 
     let varsadded = List.fold_left (fun vt x -> let r,_,_ = x in F.new_var vt (reg_to_var r)) v regList in
     List.fold_left (fun vt x -> let r,l,h = x in F.set_var vt (reg_to_var r) l h) varsadded regList
 
-  (** Sets the initial values for certain addresses in the value domain *)
+  (* Sets the initial values for certain addresses in the value domain *)
   let initVals v memList =
     let addresses_added = List.fold_left (fun vt x -> let var,_,_ = x in F.new_var vt var) v memList in
     List.fold_left (fun vt x -> let addr,l,h = x in F.set_var vt addr l h) addresses_added memList
@@ -124,7 +110,7 @@ module Make (F : FlagAD.S) (C:CacheAD.S) = struct
   let initMemory m memList =
     List.fold_left (fun m x -> let addr,_,_ = x in MemSet.add addr m) m memList
 
- (** Return an element of type t with initialized registers *)
+ (* Return an element of type t with initialized registers *)
   let init iv (memList,regList) cache_params = {
     vals = initVals (initRegs (F.init var_to_string) regList) memList;
     memory = initMemory MemSet.empty memList;
@@ -132,9 +118,10 @@ module Make (F : FlagAD.S) (C:CacheAD.S) = struct
     cache = C.init cache_params
   }
 
-
-  (* merge_with : (F.t -> F.t -> F.t) -> t -> t -> t *)
-  (** We create variables only present in one [t] and add them to the other [t]
+  
+  (*** Functions for abstract interpretation ***)
+  
+  (* We create variables only present in one [t] and add them to the other [t]
     and vice versa, in order to apply "merge" function and combine the two
     environments. *)
   let merge_with f g x y = if x == y then x else
@@ -147,13 +134,13 @@ module Make (F : FlagAD.S) (C:CacheAD.S) = struct
              cache = g x.cache y.cache
     }
 
-  (** Join using the joins of the Flag and Cache abstract domains *)
+  (* Join using the joins of the Flag and Cache abstract domains *)
   let join = merge_with F.join C.join
   
-  (** Same as join *)
+  (* Same as join *)
   let widen = merge_with F.widen C.widen
 
- (** Union of a sequence of abstract elements *)
+ (* Union of a sequence of abstract elements *)
  (* May raise Bottom *)
   let list_join = function
     [] -> raise Bottom
@@ -161,16 +148,11 @@ module Make (F : FlagAD.S) (C:CacheAD.S) = struct
 
   let subseteq x y = if x == y then true 
     else F.subseteq x.vals y.vals && MemSet.subset x.memory y.memory
-
-  let test env cond =
-    let lift = function Bot -> Bot 
-    | Nb v -> Nb {env with vals = v; cache = C.elapse env.cache time_test} 
-    in
-    let (t,f) = F.test env.vals cond in
-    lift t, lift f
-
-  (* get_reg32 : t -> X86Types.reg32 -> (int64, F.t) finite_set *)
-  (** @return the finite set of possible values or top corresponding to a register *)
+  
+  
+  (*** Functions for interpreting instructions ***)
+  
+  (* returns the finite set of possible values or top corresponding to a register *)
   let get_reg32 env r = 
     F.get_var env.vals (reg_to_var r)
 
@@ -178,7 +160,6 @@ module Make (F : FlagAD.S) (C:CacheAD.S) = struct
   let unFinite = function
     | Nt x -> NumMap.bindings x
     | Tp -> raise Is_Top
-
 
   (* Returns a list of all possible addresses and corresponding value environment *)
   (* which are described by addr. The resulting addresses are possible evaluations *)
@@ -191,18 +172,21 @@ module Make (F : FlagAD.S) (C:CacheAD.S) = struct
     let index = match addr.addrIndex with
         Some(scale,reg) ->
           let intscale = Int64.of_int (X86Util.scale_to_size scale) in
-          List.map (fun (x,e) -> (Int64.mul intscale x, e)) (unFinite (get_reg32 env reg))
+          List.map (fun (x,e) -> 
+            (Int64.mul intscale x, e)) (unFinite (get_reg32 env reg))
       | None ->  [(0L, env.vals)] in
     (* Combine all the base and index values and meet the enviroments *)
     assert(index <> []);
-    let comb = List.concat (List.map (fun (x,e) -> List.map (fun (y,e') -> (Int64.add x y, F.meet e e')) index) base) in
+    let comb = List.concat (List.map (fun (x,e) -> List.map (fun (y,e') -> 
+      (Int64.add x y, F.meet e e')) index) base) in
     List.map (fun (x, e) -> (Int64.add addr.addrDisp x, e)) comb
       
     
-  (** Create an unitialized variable; assume it is not already created. *) 
-  let create_var env n addr = {env with vals = F.new_var env.vals n; memory = MemSet.add n env.memory}
+  (* Create an unitialized variable; assume it is not already created. *) 
+  let create_var env n addr = 
+    {env with vals = F.new_var env.vals n; memory = MemSet.add n env.memory}
   
-  (** return the 32-bit register that contains the given 8-bit register *)
+  (* return the 32-bit register that contains the given 8-bit register *)
   let r8_to_r32 r = X86Util.int_to_reg32 ((X86Util.reg8_to_int r) mod 4)
   
   let op_to_op32 op = match op with
@@ -273,13 +257,15 @@ module Make (F : FlagAD.S) (C:CacheAD.S) = struct
         let addrList = get_addresses env addr in
         assert(addrList<>[]);
         List.map access_addr addrList
-      ) with Is_Top -> failwith "Top in a set of values referencing addresses, cannot continue"
+      ) with Is_Top -> 
+        failwith "Top in a set of values referencing addresses, cannot continue"
 
-  (** return the enviroment that corresponds to a memory access *)
+  (* return the enviroment that corresponds to a memory access *)
   let get_access_env d s ed es = 
     match d,s with
     | Op32(Address _), Op32(Address _) | Op8(Address _), Op8(Address _) ->
-      failwith "Memory-to-memory operation not supported" (* would currently record only one cache access *)
+      failwith "Memory-to-memory operation not supported" 
+      (* would currently record only one cache access *)
     | Op32(Address _), _ | Op8(Address _), _ -> ed
     | _, Op32(Address _) | _, Op8(Address _) -> es
     | _, _ -> ed
@@ -316,62 +302,25 @@ module Make (F : FlagAD.S) (C:CacheAD.S) = struct
     "MemAD.update_mem: Bottom in memAD after an operation on non bottom env"
   
   
-  (** Performs the Load Effective Address instruction by loading each possible
+  (* Performs the Load Effective Address instruction by loading each possible
     address in the variable correspoding to the register.
     @return an environment or raises Bottom *)
   let load_address env reg addr = let res = try( try (
     let addrList = get_addresses env addr in
     let regVar = reg_to_var reg in
-    let envList = List.map (fun (x,e) -> { env with vals = F.update_val e regVar NoMask (Cons x) NoMask Amov }) addrList in
+    let envList = List.map (fun (x,e) -> { env with vals = 
+      F.update_val e regVar NoMask (Cons x) NoMask Amov }) addrList in
     list_join envList
-  ) with Bottom -> failwith "MemAD.load_address: bottom after an operation on non bottom environment"
-  ) with Is_Top -> let regVar = reg_to_var reg in {env with vals = F.set_var env.vals regVar 0L 0xffffffffL} 
+  ) with Bottom -> failwith 
+    "MemAD.load_address: bottom after an operation on non bottom environment"
+  ) with Is_Top -> let regVar = reg_to_var reg in 
+    {env with vals = F.set_var env.vals regVar 0L 0xffffffffL} 
   in {res with cache = C.elapse res.cache time_effective_load}
   
-  
-  (** Determines a finite list of possible values given a [genop32] with each
-    value associated with the environment that led to it.
-    @return the list mentioned above or Top if there is no finite list. *)
-  (* in the resulting environments, time will have passed by an amount depending on the memory accesses *)
-  let get_vals env gop = match gop with
-    | Imm x -> Finite [(Int64.to_int x, env)]
-    | Reg r -> 
-        begin
-          (* Get values correspoding to register r. If Top is returned, we return Top;
-           * otherwise we return the values converted to int and its corresponding environment *)
-          let vals = get_reg32 env r in
-          match vals with
-            Nt x -> Finite (List.map (fun (v,e) -> (Int64.to_int v, {env with vals = e})) (NumMap.bindings x))
-          | Tp -> Top env
-        end 
-    | Address addr ->
-        let addrList = get_addresses env addr in
-        (* read - function for List.map *)
-        let read (n, e) =
-        begin
-          if MemSet.mem n env.memory
-          then
-            (* if address is in MemSet, we get the values from F *)
-            let vals = F.get_var e n in
-            match vals with
-              Nt x -> NumMap.bindings x
-            | Tp -> raise Is_Top
-          else 
-            (* if address not in MemSet, we use the initilization function to get the value *)
-            match env.initial_values n with
-              Some value -> [value, e]
-            | None -> raise Is_Top
-        end
-        in
-        (* appendLists - function for List.fold_left; return the list with the contents of each address *)
-        let appendLists r xs = (List.map (fun (x,e) -> (Int64.to_int x, {env with vals = e})) xs)@r
-        in
-        (try (
-          let fsList = List.map read addrList in
-          Finite (List.fold_left appendLists [] fsList)
-        ) with Is_Top -> Top env)
-  
   let imul env dst src imm = failwith "IMUL not implemented"
+  
+  
+  (*** Public functions ***)
   
   let rec interpret_instruction env instr = match instr with
   | Arith(op, dst, src) -> begin
@@ -390,12 +339,59 @@ module Make (F : FlagAD.S) (C:CacheAD.S) = struct
   | Movzx(dst32,src8) -> update_mem env Amov (Op32 dst32) (Op8 src8)
   | Lea(r,a) -> load_address env r a
   | Imul(dst,src,imm) -> imul env dst src imm
-  | Shift(sop,dst32,offst8) -> update_mem env (Ashift sop) (Op32 dst32) (Op8 offst8)
+  | Shift(sop,dst32,offst8) -> 
+    update_mem env (Ashift sop) (Op32 dst32) (Op8 offst8)
   | Cmp(dst, src) -> update_mem env (Aflag Acmp) (Op32 dst) (Op32 src)
   | Test(dst, src) -> update_mem env (Aflag Atest) (Op32 dst) (Op32 src)
-  | i -> Format.printf "@[Unexpected instruction %a @, in MemAD->interpret_instruction@]@." X86Print.pp_instr i;
+  | i -> Format.printf "@[Unexpected instruction %a @, 
+    in MemAD->interpret_instruction@]@." X86Print.pp_instr i;
     failwith ""
   
+  let get_vals env gop = match gop with
+    | Imm x -> Finite [(Int64.to_int x, env)]
+    | Reg r -> 
+        begin
+          (* Get values correspoding to register r. May return Top or
+             the values converted to int and their corresponding environments *)
+          let vals = get_reg32 env r in
+          match vals with
+            Nt x -> Finite (List.map (fun (v,e) -> 
+              (Int64.to_int v, {env with vals = e})) (NumMap.bindings x))
+          | Tp -> Top env
+        end 
+    | Address addr ->
+        let addrList = get_addresses env addr in
+        (* read - function for List.map *)
+        let read (n, e) =
+        begin
+          if MemSet.mem n env.memory
+          then
+            let vals = F.get_var e n in
+            match vals with
+              Nt x -> NumMap.bindings x
+            | Tp -> raise Is_Top
+          else 
+            match env.initial_values n with
+              Some value -> [value, e]
+            | None -> raise Is_Top
+        end
+        in
+        (* appendLists - function for List.fold_left; *)
+        (* return the list with the contents of each address *)
+        let appendLists r xs = (List.map (fun (x,e) -> 
+          (Int64.to_int x, {env with vals = e})) xs)@r
+        in
+        (try (
+          let fsList = List.map read addrList in
+          Finite (List.fold_left appendLists [] fsList)
+        ) with Is_Top -> Top env)
+        
+  let test env cond =
+    let lift = function Bot -> Bot 
+    | Nb v -> Nb {env with vals = v; cache = C.elapse env.cache time_test} 
+    in
+    let (t,f) = F.test env.vals cond in
+    lift t, lift f
   
   (* pass the elapsed time to the cache domain which keeps  track of it *)
   let elapse env d = {env with cache = C.elapse env.cache d}
