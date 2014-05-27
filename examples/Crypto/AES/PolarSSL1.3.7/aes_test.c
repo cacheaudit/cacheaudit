2e4ef297982fe8289b15f6eb31f53f84b63367a0
Boris Koepf <boris.koepf@gmail.com>
2014-05-27 15:21:04 +0000

    Works

--- examples/Crypto/AES/PolarSSL1.3.7/aes_test.c
+++ examples/Crypto/AES/PolarSSL1.3.7/aes_test.c
@@ -9,11 +9,24 @@ int main(){
 
   aes_context aes;
 
+  // Preloading of tables
+
+
+  
+  
+
+
   unsigned char key[8];
 
   const unsigned char input [16];
   unsigned char output[16];
 
+  // Homegrown preloading
+  aes_preloading(); 
+  
+
+  /* Currently we're using the POLAR_AES_ROM_TABLES option, which means that we're using precomputed tables. Uncommenting this option could help to avoid preloading, but the code does not go through due some "imul" operation that we don't cover.
+   */
 
   aes_setkey_enc( &aes, key, 256 );
 
