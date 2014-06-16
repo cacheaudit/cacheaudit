7ce08bcad17f56ac6b62cf789d6f3b5b67206e30
Goran Doychev <g.doychev@gmail.com>
2014-06-16 15:03:33 +0000

    bugfix: < keysize bits were allocated for key

--- examples/Crypto/AES/PolarSSL1.3.7/aes_test.c
+++ examples/Crypto/AES/PolarSSL1.3.7/aes_test.c
@@ -12,11 +12,12 @@ int main(){
   // Preloading of tables
 
 
-  
+ #define KEYSIZE 128
+//  #define KEYSIZE 256
   
 
 
-  unsigned char key[8];
+  unsigned char key[(KEYSIZE + 7)/8];
 
   const unsigned char input [16];
   unsigned char output[16];
@@ -28,7 +29,7 @@ int main(){
   /* Currently we're using the POLAR_AES_ROM_TABLES option, which means that we're using precomputed tables. Uncommenting this option could help to avoid preloading, but the code does not go through due some "imul" operation that we don't cover.
    */
 
-  aes_setkey_enc( &aes, key, 256 );
+  aes_setkey_enc( &aes, key, KEYSIZE );
 
   aes_crypt_ecb (&aes, AES_ENCRYPT, input, output);
 
