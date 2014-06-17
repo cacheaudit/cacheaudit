9c7e40ef64684df0344e1d9e9ae17342e1a49834
Goran Doychev <g.doychev@gmail.com>
2014-06-17 18:48:29 +0000

    a Makefile for building the aes executables

--- examples/Crypto/AES/PolarSSL1.3.7/aes_test.c
+++ examples/Crypto/AES/PolarSSL1.3.7/aes_test.c
@@ -1,29 +1,26 @@
 #include "config.h"
 #include "aes.h"
+#include <string.h>
+#include <stdio.h>
 
-
-
+#ifndef KEYSIZE
+  #define KEYSIZE 256
+#endif
 
 
 int main(){
 
   aes_context aes;
 
-  // Preloading of tables
-
-
- #define KEYSIZE 128
-//  #define KEYSIZE 256
-  
-
-
   unsigned char key[(KEYSIZE + 7)/8];
 
   const unsigned char input [16];
   unsigned char output[16];
 
-//   // Homegrown preloading
-//   aes_preloading(); 
+#ifdef PRELOADING
+  // Homegrown preloading
+  aes_preloading();
+#endif
   
 
   /* Currently we're using the POLAR_AES_ROM_TABLES option, which means that we're using precomputed tables. Uncommenting this option could help to avoid preloading, but the code does not go through due some "imul" operation that we don't cover.
